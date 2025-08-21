50b316312c768851d19d4c0f9faa7aae45628e89
sunwoong <sunwoong89@g.skku.edu>
2025-08-21 17:25:46 +0900

    [Feat] 게스트하우스 세부정보 API 제작

--- backend/src/main/java/com/backend/repository/GuesthouseRepository.java
+++ backend/src/main/java/com/backend/repository/GuesthouseRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.entity.Guesthouse;
 
-public interface GuesthouseRepository extends JpaRepository<Guesthouse, Long> {
+public interface GuesthouseRepository extends JpaRepository<Guesthouse, Integer> {
 }
