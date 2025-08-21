bd3994feeae525d5d5aaebbe8d5fe66c1523fa6d
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2025-08-21 15:45:19 +0900

    [feat] 사용자 정보 조회 및 수정 API 구현 (#3)

--- backend/src/main/java/com/backend/repository/GuesthouseRepository.java
+++ backend/src/main/java/com/backend/repository/GuesthouseRepository.java
@@ -1,8 +1,10 @@
 package com.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.backend.entity.Guesthouse;
 
+@Repository
 public interface GuesthouseRepository extends JpaRepository<Guesthouse, Long> {
 }
