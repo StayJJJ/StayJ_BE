0e28bbefe5ff57cc1413190681bb5932d7b5b4ad
DevJihun <kjh31021@g.skku.edu>
2025-08-22 17:15:08 +0900

    chore: CORS

--- backend/src/main/java/com/backend/config/WebConfig.java
+++ backend/src/main/java/com/backend/config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                 .allowedOrigins("http://localhost:3000") // 프론트엔드 URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
-                .allowedHeaders("*"); // 모든 헤더 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true);
     }
 }
