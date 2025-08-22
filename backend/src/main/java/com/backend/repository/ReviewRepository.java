467dfc6bdfee4aabd4447a427550047bae90b6ad
sunwoong00 <56298875+sunwoong00@users.noreply.github.com>
2025-08-22 17:15:01 +0900

    Guesthouse Feature (#12)
    * [Feat] 게스트하우스 세부정보 API 제작
    
    * [Feat] 게스트하우스 기능 추가

---
+++ backend/src/main/java/com/backend/repository/ReviewRepository.java
@@ -0,0 +1,18 @@
+package com.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.backend.entity.Review;
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    @Query("SELECT r FROM Review r " +
+           "JOIN r.reservation res " +
+           "JOIN res.room room " +
+           "WHERE room.guestHouse.id = :guesthouseId " +
+           "ORDER BY r.createdAt DESC")
+    List<Review> findByGuesthouseId(@Param("guesthouseId") Integer guesthouseId);
+}
