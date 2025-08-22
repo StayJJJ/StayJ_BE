5e7be9e81524a3f3ef8cde4f4429267d88d27d8a
sunwoong <sunwoong89@g.skku.edu>
2025-08-22 17:12:13 +0900

    [Feat] 게스트하우스 기능 추가

--- backend/src/main/java/com/backend/entity/Room.java
+++ backend/src/main/java/com/backend/entity/Room.java
@@ -16,6 +16,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -23,6 +24,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
