99a354241101707685201ddc7172737ef6d4f5f2
chaeeerish <109421279+chaeeerish@users.noreply.github.com>
2025-08-22 15:46:23 +0900

    Reservation new (#11)
    * [feat] 예약 등록 API
    
    * [feat] 예약 조회 및 삭제 API
    
    * [fix] git push 에러 해결

---
+++ backend/src/main/java/com/backend/dto/response/ReservationResponse.java
@@ -0,0 +1,19 @@
+package com.backend.dto.response;
+
+import java.time.LocalDate;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ReservationResponse {
+    private Integer id;
+    private Integer roomId;
+    private Integer guesthouseId;
+    private String guesthouseName;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private Integer peopleCount;
+    private Boolean reviewed;
+}
