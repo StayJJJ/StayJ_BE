9f45f81f3318fbd0a55d16b8a0880c3c5290dace
sunwoong <sunwoong89@g.skku.edu>
2025-08-25 14:44:15 +0900

    change structure

--- backend/src/main/java/com/backend/controller/ReservationInfoController.java
+++ backend/src/main/java/com/backend/controller/ReservationInfoController.java
@@ -1,5 +1,7 @@
 package com.backend.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +9,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.backend.dto.request.GuestHouseDetailRequest;
+import com.backend.dto.request.GuestHouseRoomRequest;
+import com.backend.dto.request.RoomResponseRequest;
+import com.backend.dto.response.ReviewResponse;
 import com.backend.service.ReservationInfoService;
 import jakarta.persistence.EntityNotFoundException;
 
@@ -24,10 +30,26 @@ public class ReservationInfoController {
     @GetMapping("/{guesthouse_Id}")
     public ResponseEntity<GuestHouseDetailRequest> getGuestHouseDetail(
             @PathVariable("guesthouse_Id") int guesthouseId) {
-    	System.out.println(guesthouseId);
-    	
 
         GuestHouseDetailRequest response = reservationInfoService.getGuestHouseDetail(guesthouseId);
         return ResponseEntity.ok(response);
     }
+    
+	@GetMapping("/{guesthouse_Id}/rooms")
+	public ResponseEntity<RoomResponseRequest> getGuestHouseRoom(
+	        @PathVariable("guesthouse_Id") int guesthouseId,
+	        @RequestParam(value = "room_available", required = false) List<Integer> roomAvailable) {
+	    
+		RoomResponseRequest response = reservationInfoService.getGuestHouseRooms(guesthouseId, roomAvailable);
+	    return ResponseEntity.ok(response);
+	}
+    	
+    @GetMapping("{guesthouse_Id}/reviews")
+    public ResponseEntity<List<ReviewResponse>> getReview(
+    		@PathVariable("guesthouse_Id") int guesthouseId) {
+    	
+    	List<ReviewResponse> response = reservationInfoService.getReview(guesthouseId);
+    	return ResponseEntity.ok(response);
+    }
+   
 }
