0fa37e114cc66e346561aed7654f15d3283a0a75
DevJihun <kjh31021@g.skku.edu>
2025-08-27 11:03:57 +0900

    fix: browser가 예전 image를 cache로 가져오는 문제 해결

--- backend/src/main/java/com/backend/config/WebConfig.java
+++ backend/src/main/java/com/backend/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -21,6 +22,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///C:/upload/");
+                .addResourceLocations("file:///C:/upload/")
+                .setCacheControl(CacheControl.noCache().mustRevalidate())
+                .resourceChain(false);
     }
 }
