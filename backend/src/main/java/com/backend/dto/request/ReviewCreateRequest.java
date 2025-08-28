4a8112460da27cbedf1a8d4fa1e9772ddd8ca098
DevJihun <kjh31021@g.skku.edu>
2025-08-28 09:38:49 +0900

    fix: review error

--- backend/src/main/java/com/backend/dto/request/ReviewCreateRequest.java
+++ backend/src/main/java/com/backend/dto/request/ReviewCreateRequest.java
@@ -5,18 +5,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ReviewCreateRequest {
-	@JsonProperty("reservationId")
-    public Integer reservationId;
-    
-    @JsonProperty("rating")
-    public Integer rating;
-    
-    @JsonProperty("comment")
-    public String comment;
-}
+	private Integer reservationId;
 
+	private Integer rating;
+
+	private String comment;
+}
